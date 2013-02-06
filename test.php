bd676eb8d9edd7f02a4fb719ec67f3e598e9bfc1
AlexanderIII <matenkov@mail.ru>
2013-02-06 12:51:13 +0100

    c1

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Created on 06.02.2013
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+ echo "GIT1";
+?>
